a8f7a57a2026eab3a6cd9b75fb66f2c8b7419de6
Rohan Patankar <rohanpatankar926@gmail.com>
2022-05-29 20:27:58 +0530

    Template created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is just an illustration how to make a realworld package using cookiecutter and pypi ',
+    author='rohan patankar',
+    license='MIT',
+)
